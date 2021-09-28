828a24b7cf9a1922e3d9385ebd4a260c2a5f74f7
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2021-09-28 23:43:03 +0530

    JavaScript

---
+++ SciptPractice/second.js
@@ -0,0 +1,77 @@
+// If-else Conditional and Switch case
+// let age1 =34;
+// let allow1;
+// if(age1>=18){
+//     allow1 = "You are eligible for voting."
+//     document.getElementById("demo").innerHTML=  allow1;
+//     // console.log("Your are eligible for voting.")
+// }
+// else{
+//     consol.log("Your are  not eligible for voting")
+// }
+
+
+// let age2 = 4;
+// if(age2>18) {
+//     let allow = "You can drink."
+//     console.log(allow);
+// }
+// else if(age2 == 2){
+//     console.log("Age is 2.");
+// }
+// else{
+//     console.log("You can drink cold drink.");
+// }
+
+
+
+// const hour = new Date().getHours(); 
+// let greeting;
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+// document.getElementById("demo").innerHTML = greeting;
+
+
+// document.getElementById("demo").innerHTML = "My First JavaScriptProgram";
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML ="Paragraph Chaanged.";
+// }
+
+
+// function Sum() {
+//     document.getElementById("sum").innerHTML= 56+44;
+// }
+// document.write(54 +65);
+
+
+
+let cars =["Audi", "BMW", "Farrari", "Mercedes", "RoyalRolls", "Honda", "MG Gloster"];
+// cars[0] ="Toyata";
+
+// Add an Element
+// cars.push("Hyundai");
+
+// document.getElementById("mycars").innerHTML= cars;
+
+// let cars = { type: "Fiat", model: "600", color: "white"};
+// document.getElementById("demo").innerHTML= "Cars owner is " + cars.owner;
+// console.log(cars);
+
+
+// Function
+
+// function myCode(p1, p2){
+//   return p1 * p2; 
+// }
+// console.log(myCode(6,7));
+
+
+// var x = call(8, 9);
+
+
+
+
