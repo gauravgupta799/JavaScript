5c8026181d96406da280ee94fe341493eceefd5d
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-06-18 22:19:38 +0530

    Drag and drop

---
+++ Drop&Drag/drag.js
@@ -0,0 +1,42 @@
+const drop = documnent.querySelector(".drop");
+const input = drop.querySelector(".drop input");
+const text = drop.querySelector(".text");
+const progress = drop.querySelector(".progress");
+
+let files = [];
+
+input.addEventListener("change", () => {
+    drop.style.display = "none";
+    upload();
+});
+
+drop.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    text.innerHTML = "Release your mouse to drop.";
+    drop.classList.add("active");
+});
+drop.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    text.innerHTML = "Drag and drop your documents, photos,and videos here...";
+    drop.classList.remove("active");
+});
+
+drop.addEventListener("drop", (e) => {
+    e.preventDefault();
+    files = e.dataTransfer.files;
+    drop.style.display = "none";
+    upload();
+});
+
+const upload = () => {
+    let intervalCount = 0.25;
+    progress.style.display = "block";
+    progress.style.width = `${20 * intervalCount}%`;
+    const interval = setInterval(() => {
+        intervalCount += 0.25;
+        progress.style.width = `${20 * intervalCount}%`;
+        if(intervalCount == 5){
+            clearInterval(interval);
+        }
+    },1000);
+}
