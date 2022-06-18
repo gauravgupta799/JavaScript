a4df2ae3939c65d6f750679014d5835de71e3d4e
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-06-18 23:02:44 +0530

    otpInputField

---
+++ OTP_Input_Field/otp.js
@@ -0,0 +1,48 @@
+const inputs = document.querySelectorAll('.otp-field input');
+
+inputs.forEach((input, index) => {
+    input.dataset.index = index;
+    input.addEventListener("paste", handleOnPasteOtp);
+    input.addEventListener("keyup", handleOtp);
+});
+
+ const handleOnPasteOtp = (e) => {
+    const data = e.clipboardData.getData("text");
+    const value = data.split("");
+    if(value.length === inputs.length) {
+        inputs.forEach((input, index) => (input.value = value[index]));
+        submit();
+    }
+ }
+
+ const handleOtp = (e) => {
+    const input = e.taget;
+    let value = input.value;
+    input.value = "";
+    input.value = value ? value[0] : "";
+
+    let fieldIndex = input.dataset.index;
+    if(value.length > 0 && fieldIndex < inputs.length - 1){
+        input.nextElementSibling.focus();
+    }
+
+    if(e.key === "Backspace" && fieldIndex > 0){
+        input.previousElementSibling.focus();
+    }
+
+    if(fieldIndex = inputs.length - 1){
+        submit()
+    }
+ }
+
+ const submit = () => {
+    console.log("Submitting....");
+    // Entered OTP
+    let otp = " ";
+    inputs.forEach((input) => {
+        otp += input.value;
+        input.disabled = true;
+        input.classList.add("disabled");
+    });
+    console.log(otp);
+ }
